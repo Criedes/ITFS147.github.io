8592fdf8fb5cf90034a79a89e93b8bb741eda7e7
Rattanachat Sooksumpus <59070147@it.kmitl.ac.th>
2018-06-18 05:09:06 +0700

    fixed init modal to one click

--- js/init.js
+++ js/init.js
@@ -18,10 +18,10 @@ tdRef.once('value', function (snapshot) {
     count += 1;
     var key = childSnapshot.key;
     var childData = childSnapshot.val();
-    if(count<25 || count > 26)
-    $('#teacher-list').append(teacherCard(childData, pad(count)));
-    else if(count == 25 || count == 26)
-    $('#officer-list').append(officerCard(childData, pad(count)));
+    if (count < 25 || count > 26)
+      $('#teacher-list').append(teacherCard(childData, pad(count)));
+    else if (count == 25 || count == 26)
+      $('#officer-list').append(officerCard(childData, pad(count)));
     //     document.querySelector('#teacher-list')
     // .innerHTML += teacherCard(childData, count);
   });
@@ -54,7 +54,7 @@ function teacherCard(teacher, count) {
   var email = teacher.email;
   html += email[0] + '<BR>';
   html += teacher.room + '</p>';
-  html += '<button onclick="teacherModal(this.id)" class="t-center more-info" id="teacher-' + count + '" value = "' + count + '" data-toggle="modal"' + 'data-target="#modal-teacher' + count + '"' + '">';
+  html += '<button class="t-center more-info" id="teacher-' + count + '" value = "' + count + '" data-toggle="modal"' + 'data-target="#modal-teacher' + count + '"' + '">';
   html += 'ดูข้อมูลเพิ่มเติม<i class="fa fa-long-arrow-right m-l-10" aria-hidden="true"></i>';
   html += '</button>'
   html += '</div>';
@@ -62,104 +62,107 @@ function teacherCard(teacher, count) {
   html += '</div>';
   return html;
 }
-
-//Modal when you click more info
-function teacherModal(id) {
-  tdRef.on('value', function (snapshot) {
-    var x = document.getElementById(id).getAttribute("value");
-    var childData = snapshot.child('user' + x).val();
-    $('#modal-every-teacher').append(createModal(childData, x));
+tdRef.on('value', function (snapshot) {
+  count = 0;
+  snapshot.forEach(function (childSnapshot) {
+    count += 1;
+    var key = childSnapshot.key;
+    var childData = childSnapshot.val();
+    $('#modal-every-teacher').append(createModal(childData, pad(count)));
   });
-  function createModal(teacher, count) {
-    var html = '';
-    var fileName = 'user' + count + '.jpg';
-    var imagesRef = 'user_pic%2F' + fileName;
-    var urlIm = 'https://firebasestorage.googleapis.com/v0/b/math-web-kmitl.appspot.com/o/' + imagesRef + '?alt=media';
-    html += '<div class="modal fade modal-teacher" id="modal-teacher'+count+'" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">';
-    html += '<div class="modal-dialog modal-lg" role="document">';
-    html += '<div class="modal-content">';
-    html += '<div class="modal-header">';
-    html += '<h5 class="modal-title" id="myLargeModalLabel">ข้อมูลบุคลากร</h5>';
-    html += '</div>';
-    html += '<div class="modal-body">';
-    html += '<section class="dados">';
-    html += '<img src="'+urlIm+'" id="profile-img">';
-    html += '<section class="user-info">';
-    html += '<h3 id="username" style="color:black;">ชื่อ-นามสกุล</h3>';
-    html += '<h1 class="title-name" id="title" style="color: #212529;">' + teacher.title + '</h1>';
-    html += '<h1 class="title-name" id="firstname" style="color: #212529;">' + teacher.name + '</h1>';
-    html += '<h1 class="title-name" id="lastname" style="color: #212529;">' + teacher.surname + '</h1><BR>';
-    html += '<h1 class="title-name" id="title_en" style="color: #212529;">' + teacher.title_en + '</h1>';
-    html += '<h1 class="title-name" id="firstname_en" style="color: #212529;">' + teacher.name_en + '</h1>';
-    html += '<h1 class="title-name" id="lastname_en" style="color: #212529;">' + teacher.surname_en + '</h1><BR>';
-    html += '</section>';
-    html += '<h3 id="bio-text" style="color:black;">อีเมลล์</h3>';
-    html += '<section class="user-bio">';
-    var email = teacher.email;
-    for(i=0;i<email.length;i++){
-      html += ' - '+email[i]+'<BR>';
-    }
-    html += '</section>';
-    html += '<h3 style="color:black;">โทรศัพท์</h3>';
-    html += '<section class="user-bio">';
-    var tel = teacher.tel;
-    for(i=0;i<tel.length;i++){
-      html += ' - '+tel[i]+'<BR>';
-    }
-    html += '</section>';
-    html += '<h3 style="color:black;">ห้องพัก</h3>';
-    html += '<section class="user-bio">';
-    html +=  ' - '+teacher.room;
-    html += '</section>';
-    html += '<h3 style="color:black;">โฮมเพจ</h3>';
-    html += '<section class="user-bio">';
-    if(teacher.homepage == '-'){
-      html +=  ' - ';
-    }
-    else{
-      html +=  ' - '+teacher.homepage;
-    }
-    html += '</section>';
-    html += '<h3 style="color:black;">การศึกษา</h3>';
-    html += '<section class="user-bio">';
-    var education = teacher.education;
-    for(i=0;i<education.length;i++){
-      if(education[i] != '-')
-        html += ' - '+education[i]+'<BR>';
-    }
-    html += '</section>';
-    html += '<h3 style="color:black;">สาขาที่เชี่ยวชาญ/สนใจ</h3>';
-    html += '<section class="user-bio">';
-    var specialized_interests = teacher.specialized_interests;
-    for(i=0;i<specialized_interests.length;i++){
-      if(specialized_interests[i] != '-')
-        html += ' - '+specialized_interests[i]+'<BR>';
-    }
-    html += '</section>';
-    html += '<h3 style="color:black;">งานวิจัย/สิ่งตีพิมพ์</h3>';
-    html += '<section class="user-bio">';
-    var research = teacher.research;
-    for(i=0;i<research.length;i++){
-      if(research[i] != '-')
-        html += ' - '+research[i]+'<BR>';
-    }
-    html += '</section>';
-    html += '<h3 style="color:black;">รายวิชาที่รับผิดชอบ</h3>';
-    html += '<section class="user-bio">';
-    var responsible_course = teacher.responsible_course;
-    for(i=0;i<responsible_course.length;i++){
-      if(responsible_course[i] != '-')
-        html += ' - '+responsible_course[i]+'<BR>';
-    }
-    html += '</section>';
-    html += '</section>';
-    html += '</div>';
-    html += '<div class="modal-footer"><button type="button" class="btn btn-primary" data-dismiss="modal">ปิด</button></div>';
-    html += '</div></div></div>';
-    return html;
-  }
 
+});
+//Modal when you click more info
+
+function createModal(teacher, count) {
+  var html = '';
+  var fileName = 'user' + count + '.jpg';
+  var imagesRef = 'user_pic%2F' + fileName;
+  var urlIm = 'https://firebasestorage.googleapis.com/v0/b/math-web-kmitl.appspot.com/o/' + imagesRef + '?alt=media';
+  html += '<div class="modal fade modal-teacher" id="modal-teacher' + count + '" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">';
+  html += '<div class="modal-dialog modal-lg" role="document">';
+  html += '<div class="modal-content">';
+  html += '<div class="modal-header">';
+  html += '<h5 class="modal-title" id="myLargeModalLabel">ข้อมูลบุคลากร</h5>';
+  html += '</div>';
+  html += '<div class="modal-body">';
+  html += '<section class="dados">';
+  html += '<img src="' + urlIm + '" id="profile-img">';
+  html += '<section class="user-info">';
+  html += '<h3 id="username" style="color:black;">ชื่อ-นามสกุล</h3>';
+  html += '<h1 class="title-name" id="title" style="color: #212529;">' + teacher.title + '</h1>';
+  html += '<h1 class="title-name" id="firstname" style="color: #212529;">' + teacher.name + '</h1>';
+  html += '<h1 class="title-name" id="lastname" style="color: #212529;">' + teacher.surname + '</h1><BR>';
+  html += '<h1 class="title-name" id="title_en" style="color: #212529;">' + teacher.title_en + '</h1>';
+  html += '<h1 class="title-name" id="firstname_en" style="color: #212529;">' + teacher.name_en + '</h1>';
+  html += '<h1 class="title-name" id="lastname_en" style="color: #212529;">' + teacher.surname_en + '</h1><BR>';
+  html += '</section>';
+  html += '<h3 id="bio-text" style="color:black;">อีเมลล์</h3>';
+  html += '<section class="user-bio">';
+  var email = teacher.email;
+  for (i = 0; i < email.length; i++) {
+    html += ' - ' + email[i] + '<BR>';
+  }
+  html += '</section>';
+  html += '<h3 style="color:black;">โทรศัพท์</h3>';
+  html += '<section class="user-bio">';
+  var tel = teacher.tel;
+  for (i = 0; i < tel.length; i++) {
+    html += ' - ' + tel[i] + '<BR>';
+  }
+  html += '</section>';
+  html += '<h3 style="color:black;">ห้องพัก</h3>';
+  html += '<section class="user-bio">';
+  html += ' - ' + teacher.room;
+  html += '</section>';
+  html += '<h3 style="color:black;">โฮมเพจ</h3>';
+  html += '<section class="user-bio">';
+  if (teacher.homepage == '-') {
+    html += ' - ';
+  }
+  else {
+    html += ' - ' + teacher.homepage;
+  }
+  html += '</section>';
+  html += '<h3 style="color:black;">การศึกษา</h3>';
+  html += '<section class="user-bio">';
+  var education = teacher.education;
+  for (i = 0; i < education.length; i++) {
+    if (education[i] != '-')
+      html += ' - ' + education[i] + '<BR>';
+  }
+  html += '</section>';
+  html += '<h3 style="color:black;">สาขาที่เชี่ยวชาญ/สนใจ</h3>';
+  html += '<section class="user-bio">';
+  var specialized_interests = teacher.specialized_interests;
+  for (i = 0; i < specialized_interests.length; i++) {
+    if (specialized_interests[i] != '-')
+      html += ' - ' + specialized_interests[i] + '<BR>';
+  }
+  html += '</section>';
+  html += '<h3 style="color:black;">งานวิจัย/สิ่งตีพิมพ์</h3>';
+  html += '<section class="user-bio">';
+  var research = teacher.research;
+  for (i = 0; i < research.length; i++) {
+    if (research[i] != '-')
+      html += ' - ' + research[i] + '<BR>';
+  }
+  html += '</section>';
+  html += '<h3 style="color:black;">รายวิชาที่รับผิดชอบ</h3>';
+  html += '<section class="user-bio">';
+  var responsible_course = teacher.responsible_course;
+  for (i = 0; i < responsible_course.length; i++) {
+    if (responsible_course[i] != '-')
+      html += ' - ' + responsible_course[i] + '<BR>';
+  }
+  html += '</section>';
+  html += '</section>';
+  html += '</div>';
+  html += '<div class="modal-footer"><button type="button" class="btn btn-all" data-dismiss="modal">ปิด</button></div>';
+  html += '</div></div></div>';
+  return html;
 }
+
 
 //officer card 
 function officerCard(teacher, count) {
